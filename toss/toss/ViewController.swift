682601373365c550d2b3042cecb089a58d335ade
csm3907 <csm5327@hanmail.net>
2025-01-31 13:25:20 +0900

    [Toss Sample 생성]

---
+++ toss/toss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toss
+//
+//  Created by 최승민 on 1/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ toss/tossTests/tossTests.swift
@@ -0,0 +1,17 @@
+//
+//  tossTests.swift
+//  tossTests
+//
+//  Created by 최승민 on 1/31/25.
+//
+
+import Testing
+@testable import toss
+
+struct tossTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
